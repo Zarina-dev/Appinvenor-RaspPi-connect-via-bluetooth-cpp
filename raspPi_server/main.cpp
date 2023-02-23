9c38a0f5f47a2b35b2b52537cb9ec3e0fb4ee14b
Zarina <61898376+Zarina-dev@users.noreply.github.com>
2023-02-23 19:37:56 +0900

    Update main.cpp

--- raspPi_server/main.cpp
+++ raspPi_server/main.cpp
@@ -1,18 +1,5 @@
 // ref: https://webnautes.tistory.com/1137
 #include "defines.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <bluetooth/bluetooth.h>  // sudo apt-get install libbluetooth-dev
-#include <bluetooth/sdp.h>
-#include <bluetooth/sdp_lib.h>
-#include <bluetooth/rfcomm.h>
-#include <sys/wait.h>
-#include <pthread.h>
-#include <signal.h>
 
 
 
@@ -105,7 +92,7 @@ void *ThreadMain(void* argument)
         
         char *recv_message = read_server(client);
         if ( recv_message == NULL ){
-            printf("client disconnected\n");
+            printf("client disconnected\n");      // mobileDevice's disconnect btn clicked
             break;
         } 
         
